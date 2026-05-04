8fc8d06dc9ff55c3754fa4cc387f6c178223d35b
MosefSanogo <mosefsanogo366@gmail.com>
2026-05-04 15:13:17 +0200

    Version corrigée 1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Le "base" à la racine est le plus important pour le déploiement
-  base: '/kumar/', 
+  base: '/', 
   
   server: {
     host: '0.0.0.0', 
